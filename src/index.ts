b3c35c1d70aa6ddee02e0466637d6545703686b1
Daniel Uhm <danieluhm2004@gmail.com>
2020-01-16 20:04:58 +0900

    Change: Github Pages -> TypeScript (Completed)

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,26 @@ app.use(Morgan('dev'));
 app.set('view engine', 'pug'); // (1)
 app.set('views', './src/views'); // (2)
 app.locals.pretty = true;
+app.disable('x-powered-by');
+app.disable('etag');
 
 const path = `${__dirname}/middlewares/`;
 Fs.readdir(path, async (err, middlewares) => {
+  if (err) return console.log('⚠️  - Cannot read middlewares.');
   for (const middleware of middlewares) {
-    const importLib = await import(path + middleware);
-    app.use(importLib.default);
+    try {
+      const importLib = await import(path + middleware);
+      app.use(importLib.default);
+    } catch (e) {}
   }
 
-  app.use('/', Express.static('public'));
-  app.all('*', (req, res) => res.status(404).render('errors/404'));
+  app.get('/scripting', (req, res) => res.render('scripting', req));
+  app.use('/assets', Express.static('public'));
+  app.all('/assets/*', (req, res) => res.redirect('/assets/background.jpg'));
+  app.all('*', (req, res) => res.render('index', req));
   app.listen(3000, () => {
-    console.log('🎬 Server Has Been Enabled.');
-    console.log('📌 https://p0roro.ga');
+    console.log('💡  - Server Has Been Enabled.');
+    console.log('🚥  - Network: https://p0roro.ga');
   });
 });
+``;
